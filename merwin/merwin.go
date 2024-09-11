05fab1c8eb7ce045f632374bd981e19e3e8e563b
Bernhard Haubold <haubold@evolbio.mpg.de>
2024-09-11 14:54:19 +0200

    Made merwin more generic
    It used to be tailored to analyzing the output of macle, but window
    merging is of interest in any sliding window analysis. So in the new
    version neither macle nor the match complexity, C_m, calculated with it
    are mentioned.

--- merwin/merwin.go
+++ merwin/merwin.go
@@ -16,6 +16,7 @@ func parse(r io.Reader, args ...interface{}) {
 	w := args[0].(int)
 	threshold := args[1].(float64)
 	minimize := args[2].(bool)
+	missing := args[3].(float64)
 	sum := 0.0
 	n := 0
 	is := -1
@@ -29,9 +30,9 @@ func parse(r io.Reader, args ...interface{}) {
 		util.Check(err)
 		ws := int((mi - float64(w)/2.0) + 1.0)
 		we := int(mi + float64(w)/2.0)
-		cm, err := strconv.ParseFloat(fields[2], 64)
+		x, err := strconv.ParseFloat(fields[2], 64)
 		util.Check(err)
-		if cm < 0 {
+		if x == missing {
 			continue
 		}
 		if is >= 0 {
@@ -46,11 +47,11 @@ func parse(r io.Reader, args ...interface{}) {
 		}
 		isCand := false
 		if !minimize {
-			if cm >= threshold {
+			if x >= threshold {
 				isCand = true
 			}
 		} else {
-			if cm <= threshold {
+			if x <= threshold {
 				isCand = true
 			}
 		}
@@ -59,7 +60,7 @@ func parse(r io.Reader, args ...interface{}) {
 			if is < 0 {
 				is = ws
 			}
-			sum += cm
+			sum += x
 			n++
 			if ws > ie {
 				is = ws
@@ -76,24 +77,24 @@ func parse(r io.Reader, args ...interface{}) {
 func main() {
 	clio.PrepLog("merwin")
 	u := "merwin -t <threshold> [option]... [file]..."
-	p := "Merge overlapping windows returned by macle."
-	e := "merwin -t 0.9954 hs.cm"
+	p := "Merge overlapping windows."
+	e := "merwin -t 0.9954 hs.dat"
 	clio.Usage(u, p, e)
 	optV := flag.Bool("v", false, "version")
 	optW := flag.Int("w", 10000, "window length")
-	optT := flag.Float64("t", 0.0, "C_m threshold")
-	m := "minimizing, i. e. merge if C_m <= t " +
-		"(default maximizing, i. e. merge if C_m >= t)"
+	optT := flag.Float64("t", 0.0, "threshold")
+	m := "minimizing, i. e. merge if x_i <= t " +
+		"(default maximizing, i. e. merge if x_i >= t)"
 	optI := flag.Bool("i", false, m)
+	optM := flag.Float64("m", -1, "missing data")
 	flag.Parse()
 	if *optV {
 		util.PrintInfo("merwin")
 	}
 	if *optT == 0.0 {
-		m := "please supply a C_m threshold; this "
-		m += "can be calculated with the program mantile "
+		m := "please supply a threshold"
 		log.Fatal(m)
 	}
 	files := flag.Args()
-	clio.ParseFiles(files, parse, *optW, *optT, *optI)
+	clio.ParseFiles(files, parse, *optW, *optT, *optI, *optM)
 }
